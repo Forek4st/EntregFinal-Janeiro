e9069aea85a7d4d1fd6d82f13cc5f90a49c9482d
“Forek4st” <janeirojm91@gmail.com>
2025-01-24 14:32:43 -0600

    Add Model Y product to ProductContainer and improve formatting in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import NavBar from "./components/navbar/NavBar.jsx"
-import ProductContainer from "./components/products/ProductContainer.jsx"
-import model3 from './assets/model3.png'
+import NavBar from "./components/navbar/NavBar.jsx";
+import ProductContainer from "./components/products/ProductContainer.jsx";
+import model3 from "./assets/model3.png";
+import modely from "./assets/modely.png";
 
 const App = () => {
-
   return (
     <>
       <NavBar />
-     <main>
-      
-     <ProductContainer title='Model 3' text='Gracias a un exterior actualizado y optimizado para maximizar la eficiencia aerodinámica, podrás recorrer hasta 548 kilómetros (est. EPA) con una sola carga.' img={model3}/>
-     <ProductContainer title='Loco Perdido' text='Este vato esta loco' img='https://scontent.fmex10-1.fna.fbcdn.net/v/t39.30808-6/474208825_1027317002774209_3338563256523699490_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGorpSaVnL8_tKG-f6yDLK93iLMHtcfVvHeIswe1x9W8Z9euTFrFMLliBDSDTnURJ8&_nc_ohc=qL5fttKr8uYQ7kNvgFpu57J&_nc_zt=23&_nc_ht=scontent.fmex10-1.fna&_nc_gid=ANObfF91fQ6XxU1-a3E33jc&oh=00_AYCxnOaYvETl5A2bk8L3vWEO0nEVR-GfexIRonzTx4APmw&oe=67998A14'/>
-      
+      <main>
+        <ProductContainer
+          title="Model 3"
+          text="Gracias a un exterior actualizado y optimizado para maximizar la eficiencia aerodinámica, podrás recorrer hasta 548 kilómetros (est. EPA) con una sola carga."
+          img={model3}
+        />
+        <ProductContainer
+          title="Model Y"
+          text="Disfruta de tu viaje en carretera. Con hasta 497 km (est. EPA) de autonomía con una sola carga, es probable que necesites un descanso antes que tu Model Y."
+          img={modely}
+        />
       </main>
     </>
-   
-  )
-}
+  );
+};
 
-export default App
+export default App;
