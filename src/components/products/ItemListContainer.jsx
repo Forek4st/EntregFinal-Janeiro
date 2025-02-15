1a6065c043bb622b3ce98ab2cc15a51454dd14ca
Forek4st <janeirojm91@gmail.com>
2025-02-15 11:18:40 -0600

    Enhance responsive design for item lists and detail containers; implement grid layout for larger screens

--- src/components/products/ItemListContainer.jsx
+++ src/components/products/ItemListContainer.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
 import ItemList from "../itemlist/ItemList.jsx";
 import { getProducts } from "../../asyncMock.js";
